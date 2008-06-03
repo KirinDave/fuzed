063a3316f1d2fc5cb7bfb1e46ee6c725aa5bdd2e
Abhay Kumar <abhay+git@opensynapse.net>
2008-06-03 00:26:54 -0700

    removing status thingy

--- elibs/master_server.erl
+++ elibs/master_server.erl
@@ -19,8 +19,6 @@ request_loop(Req, DocRoot) ->
       mochiweb_rpc:handler(Req, {master_responder, rpc_handler});
     {'POST', _} ->
       Req:not_found();
-    {Method, "status"} when Method =:= 'GET'; Method =:= 'HEAD' ->
-      Req:not_found();
     {Method, _} when Method =:= 'GET'; Method =:= 'HEAD' ->
       Req:serve_file(Path, DocRoot);
     {_, _} ->
