974dfdfab9fe11d9fe4355249c04d0e59d473c51
David Fayram <dfayram@gmail.com>
2009-04-29 11:08:09 -0700

    removing some powerset specific data from tests.

--- etest/node_api_test.erl
+++ etest/node_api_test.erl
@@ -25,22 +25,3 @@ nested_struct_array_conversion_test() ->
             {restrict_to_url, {struct, [{key, <<"Test me!">>}]}}]}}],
   V2 = prepare_parameters(V1).
   
-realistic_parameter_test() -> 
-  V1 = [{options,{struct,[{sem_laxer_pseudo_coefficient,"9"},
-                                {sem_loose_default_role_coeff,"1.0"},
-                                {sem_semor_expand_hierarchy,"0.5"},
-                                {title_redirect_score,"0.0"},
-                                {kw_max_keyphrase_length,"5"},
-                                {kw_keyphrase_full_query,"true"},
-                                {kw_pseudo_stopword_relative_weight,"0.7"}]}},
-              {passage,"Why doesn't this work?"}],
-  V2 = [{options,{struct,[{sem_laxer_pseudo_coefficient,<<"9">>},
-                          {sem_loose_default_role_coeff,<<"1.0">>},
-                          {sem_semor_expand_hierarchy,<<"0.5">>},
-                          {title_redirect_score,<<"0.0">>},
-                          {kw_max_keyphrase_length,<<"5">>},
-                          {kw_keyphrase_full_query,<<"true">>},
-                          {kw_pseudo_stopword_relative_weight,<<"0.7">>}]}},
-              {passage,<<"Why doesn't this work?">>}],
-  V2 = prepare_parameters(V1).
-        
