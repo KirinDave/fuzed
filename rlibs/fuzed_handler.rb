56098d9e4c139613845289bdd5acebdfe608981a
AOK <aok@chariotsolutions.com>
2009-08-11 17:04:19 -0400

    resolve undefined method 'each_line' error

--- rlibs/fuzed_handler.rb
+++ rlibs/fuzed_handler.rb
@@ -121,7 +121,7 @@ module Rack
           res = 
           [:response,
            [[:status, status.to_i],
-            [:allheaders, headers.inject([]) { |a, x| a += x[1].each_line.map { |y| [:header, x[0], y] } }],
+            [:allheaders, headers.keys.inject([]) {|x,y| x << [:header, y, headers[y]]}],
             [:html, html]]]
         rescue => e
           res = 
